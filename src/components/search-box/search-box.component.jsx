cda933ee43f1134fca04294f4d9ffc28ab973d49
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2019-07-18 11:56:35 -0500

    adding to gh pages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './search-box.styles.css'
+
+const SearchBox = ({ placeholder, handleChange}) => (
+    <input className="search" type="search" placeholder={placeholder} onChange={handleChange} />
+    
+)
+
+export default SearchBox
